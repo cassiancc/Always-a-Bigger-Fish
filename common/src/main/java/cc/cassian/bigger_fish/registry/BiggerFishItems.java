bbb2538810b28ca287a67a91fc1febfb6433745d
Cassian Godsted <cassian@godsted.com>
2025-05-27 17:32:38 -0400

    more fish

--- common/src/main/java/cc/cassian/bigger_fish/registry/BiggerFishItems.java
+++ common/src/main/java/cc/cassian/bigger_fish/registry/BiggerFishItems.java
@@ -19,21 +19,29 @@ public class BiggerFishItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, Registries.ITEM);
 
     public static DeferredSupplier<Item> BASS = createFish("bass");
+    public static DeferredSupplier<Item> BUTTERFLYFISH = createFish("butterflyfish");
     public static DeferredSupplier<Item> BLUEGILL = createFish("bluegill");
     public static DeferredSupplier<Item> CARP = createFish("carp");
+    public static DeferredSupplier<Item> CHAR = createFish("char");
     public static DeferredSupplier<Item> FLOUNDER = createFish("flounder");
+    public static DeferredSupplier<Item> GOLDEYE = createFish("goldeye");
     public static DeferredSupplier<Item> GOLDFISH = createFish("goldfish");
     public static DeferredSupplier<Item> GROUPER = createFish("grouper");
+    public static DeferredSupplier<Item> HADDOCK = createFish("haddock");
     public static DeferredSupplier<Item> HERRING = createFish("herring");
     public static DeferredSupplier<Item> KOI = createFish("koi");
     public static DeferredSupplier<Item> MACKEREL = createFish("mackerel");
     public static DeferredSupplier<Item> PERCH = createFish("perch");
+    public static DeferredSupplier<Item> PIKE = createFish("pike");
     public static DeferredSupplier<Item> SARDINE = createFish("sardine");
     public static DeferredSupplier<Item> SOULFISH = createFish("soulfish");
+    public static DeferredSupplier<Item> SURGEONFISH = createFish("surgeonfish");
     public static DeferredSupplier<Item> SWORDFISH = createFish("swordfish");
     public static DeferredSupplier<Item> TILAPIA = createFish("tilapia");
     public static DeferredSupplier<Item> TROUT = createFish("trout");
     public static DeferredSupplier<Item> TUNA = createFish("tuna");
+    public static DeferredSupplier<Item> WALLEYE = createFish("walleye");
+
 
     public static DeferredSupplier<Item> WORM = createFish("worm");
     public static DeferredSupplier<Item> LEECH = ITEMS.register("leech", ()->new LeechItem(properties("leech").arch$tab(CreativeModeTabs.FOOD_AND_DRINKS)));
