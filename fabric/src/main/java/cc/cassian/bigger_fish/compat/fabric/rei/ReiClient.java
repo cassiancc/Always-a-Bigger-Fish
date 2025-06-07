2a5eb7ecfd3d7840b04cfedacc8b34d749148d11
Cassian Godsted <cassian@godsted.com>
2025-06-07 13:28:27 -0400

    REI info recipes

---
+++ fabric/src/main/java/cc/cassian/bigger_fish/compat/fabric/rei/ReiClient.java
@@ -0,0 +1,24 @@
+package cc.cassian.bigger_fish.compat.fabric.rei;
+
+import cc.cassian.bigger_fish.registry.BiggerFishTags;
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+import me.shedaniel.rei.plugin.common.displays.DefaultInformationDisplay;
+import net.minecraft.network.chat.Component;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+public class ReiClient implements REIClientPlugin {
+
+    @Override
+    public void registerDisplays(DisplayRegistry registry) {
+        for (TagKey<Item> itemTagKey : BiggerFishTags.FISHING_TAGS_FOR_DISPLAY) {
+            addTagInfo(registry, itemTagKey);
+        }
+    }
+
+    private static void addTagInfo(DisplayRegistry registry, TagKey<Item> itemTag) {
+        registry.add(DefaultInformationDisplay.createFromEntries(EntryIngredients.ofItemTag(itemTag), Component.translatable("tag."+ itemTag.location().toLanguageKey())).lines(Component.translatable("tag."+ itemTag.location().toLanguageKey() + ".eiv")));
+    }
+}
---
+++ neoforge/src/main/java/cc/cassian/bigger_fish/compat/rei/ReiClient.java
@@ -0,0 +1,28 @@
+package cc.cassian.bigger_fish.compat.rei;
+
+import cc.cassian.bigger_fish.registry.BiggerFishTags;
+import me.shedaniel.rei.api.client.entry.filtering.FilteringResult;
+import me.shedaniel.rei.api.client.entry.filtering.FilteringResultFactory;
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+import me.shedaniel.rei.forge.REIPluginClient;
+import me.shedaniel.rei.plugin.common.displays.DefaultInformationDisplay;
+import net.minecraft.network.chat.Component;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+@REIPluginClient
+public class ReiClient implements REIClientPlugin {
+
+    @Override
+    public void registerDisplays(DisplayRegistry registry) {
+        for (TagKey<Item> itemTagKey : BiggerFishTags.FISHING_TAGS_FOR_DISPLAY) {
+            addTagInfo(registry, itemTagKey);
+        }
+    }
+
+    private static void addTagInfo(DisplayRegistry registry, TagKey<Item> itemTag) {
+        registry.add(DefaultInformationDisplay.createFromEntries(EntryIngredients.ofItemTag(itemTag), Component.translatable("tag."+ itemTag.location().toLanguageKey())).lines(Component.translatable("tag."+ itemTag.location().toLanguageKey() + ".eiv")));
+    }
+}
