c416f706a9083f39baef2bab6e8106014f381db4
Cassian Godsted <cassian@godsted.com>
2025-07-08 18:44:46 -0400

    Fix durability enchantment handling

--- common/src/main/java/cc/cassian/bigger_fish/mixin/fishing_hook/RemoveBaitMixin.java
+++ common/src/main/java/cc/cassian/bigger_fish/mixin/fishing_hook/RemoveBaitMixin.java
@@ -3,6 +3,8 @@ package cc.cassian.bigger_fish.mixin.fishing_hook;
 import cc.cassian.bigger_fish.BiggerFishMod;
 import cc.cassian.bigger_fish.registry.BiggerFishTags;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.projectile.FishingHook;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.BundleContents;
@@ -16,6 +18,7 @@ public class RemoveBaitMixin {
     @Inject(method = "retrieve", at = @At(value = "RETURN", target = "Lnet/minecraft/world/entity/item/ItemEntity;<init>(Lnet/minecraft/world/level/Level;DDDLnet/minecraft/world/item/ItemStack;)V"))
     private void removeBait(ItemStack fishingRod, CallbackInfoReturnable<Integer> cir) {
         if (fishingRod.has(DataComponents.BUNDLE_CONTENTS)) {
+            var hook = (FishingHook) (Object) this;
             BundleContents bundleContents = fishingRod.get(DataComponents.BUNDLE_CONTENTS);
             if (bundleContents != null && !bundleContents.isEmpty()) {
                 BundleContents.Mutable mutable = new BundleContents.Mutable(bundleContents);
@@ -28,8 +31,11 @@ public class RemoveBaitMixin {
                     if (itemStack.isDamageableItem()) {
                         int damageValue = itemStack.getDamageValue();
                         BiggerFishMod.LOGGER.info(damageValue);
-                        if (!itemStack.nextDamageWillBreak()) {
-                            itemStack.setDamageValue(damageValue+1);
+                        if (hook.level() instanceof ServerLevel serverLevel) {
+                            ServerPlayer owner = null;
+                            if (hook.getPlayerOwner() instanceof ServerPlayer serverPlayer)
+                                owner = serverPlayer;
+                            itemStack.hurtAndBreak(1, serverLevel, owner, (item)->{});
                             mutable.tryInsert(itemStack);
                         }
                     }
