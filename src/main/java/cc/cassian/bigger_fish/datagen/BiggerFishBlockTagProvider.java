c928581685f4676d47868e8ef809912e3091b677
Cassian <cassian@godsted.com>
2026-03-22 22:05:06 -0400

    tag fish traps correctly

--- src/main/java/cc/cassian/bigger_fish/datagen/BiggerFishBlockTagProvider.java
+++ src/main/java/cc/cassian/bigger_fish/datagen/BiggerFishBlockTagProvider.java
@@ -18,7 +18,7 @@ public class BiggerFishBlockTagProvider extends FabricTagsProvider.BlockTagsProv
 
 	@Override
 	protected void addTags(HolderLookup.Provider provider) {
-		valueLookupBuilder(BlockTags.MINEABLE_WITH_AXE).add(BiggerFishBlocks.FISH_BARREL);
+		valueLookupBuilder(BlockTags.MINEABLE_WITH_AXE).add(BiggerFishBlocks.FISH_BARREL).add(BiggerFishBlocks.FISH_TRAP);
 		valueLookupBuilder(ConventionalBlockTags.WOODEN_BARRELS).add(BiggerFishBlocks.FISH_BARREL);
 	}
 }
