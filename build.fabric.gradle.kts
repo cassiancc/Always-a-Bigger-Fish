31116e3852ffc550c3bd48d12bfee578c5883db5
Cassian <cassian@godsted.com>
2026-06-27 13:04:10 -0400

    fix easy shulker boxes

--- build.fabric.gradle.kts
+++ build.fabric.gradle.kts
@@ -186,9 +186,9 @@ dependencies {
     runtimeOnly("cc.cassian.rrv:reliable-recipe-viewer-fabric:${property("deps.rrv")}")
     compileOnly("me.shedaniel:RoughlyEnoughItems-api-neoforge:${property("deps.rei")}")
     compileOnly("me.shedaniel:RoughlyEnoughItems-default-plugin-neoforge:${property("deps.rei")}")
-    compileOnly("mezz.jei:jei-1.21.11-neoforge:${property("deps.jei")}")
+    compileOnly("mezz.jei:jei-26.1.2-fabric:${property("deps.jei")}")
 
-    compileOnly("fuzs.iteminteractions:iteminteractions-neoforge:${property("deps.iteminteractions")}")
+    implementation("fuzs.iteminteractions:iteminteractions-fabric:${property("deps.iteminteractions")}")
     // Development QOL
 //    runtimeOnly("cc.cassian.item-descriptions:item-descriptions-fabric:${property("deps.item_descriptions")}") {
 //        isTransitive = false
@@ -251,7 +251,7 @@ publishMods {
     additionalFiles.from(tasks.named<org.gradle.jvm.tasks.Jar>("sourcesJar").map { it.archiveFile.get() })
 
     // one of BETA, ALPHA, STABLE
-    type = STABLE
+    type = BETA
     displayName = "${property("mod.name")} ${property("mod.version")} for ${stonecutter.current.version} Fabric"
     version = "${property("mod.version")}+${property("deps.minecraft")}-fabric"
     changelog = provider { rootProject.file("CHANGELOG-LATEST.md").readText() }
--- src/main/java/cc/cassian/bigger_fish/compat/iteminteractions/FishBarrelProvider.java
+++ src/main/java/cc/cassian/bigger_fish/compat/iteminteractions/FishBarrelProvider.java
@@ -3,43 +3,33 @@ package cc.cassian.bigger_fish.compat.iteminteractions;
 import cc.cassian.bigger_fish.registry.BiggerFishTags;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import fuzs.iteminteractions.api.v1.DyeBackedColor;
-import fuzs.iteminteractions.api.v1.provider.impl.BundleProvider;
+import fuzs.iteminteractions.common.api.v2.world.item.DyeBackedColor;
+import fuzs.iteminteractions.common.api.v2.world.item.storage.BundleContentsStorage;
+import fuzs.iteminteractions.common.api.v2.world.item.storage.ItemStorageType;
+import fuzs.iteminteractions.common.api.v2.world.item.storage.StorageOptions;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
 
 import java.util.Optional;
 
-public class FishBarrelProvider extends BundleProvider {
-    public static final MapCodec<BundleProvider> CODEC = RecordCodecBuilder.mapCodec(instance ->
-            instance.group(capacityMultiplierCodec(), backgroundColorCodec(), itemContentsCodec())
-                    .apply(instance,
-                            (Integer capacityMultiplier, Optional<DyeBackedColor> dyeColor, ItemContents itemContents) ->
-                                    new FishBarrelProvider(capacityMultiplier, dyeColor.orElse(null)).itemContents(itemContents)
+public class FishBarrelProvider extends BundleContentsStorage {
+    public static final MapCodec<BaitedRodProvider> CODEC = RecordCodecBuilder.mapCodec(instance -> {
+        return instance.group(capacityMultiplierCodec(), itemContentsCodec())
+                .apply(instance, BaitedRodProvider::new);
+    });
 
-                    ));
-
-    public FishBarrelProvider() {
-        super(DyeBackedColor.fromDyeColor(DyeColor.BROWN));
-    }
-
-    public FishBarrelProvider(Integer capacityMultiplier, DyeBackedColor dyeBackedColor) {
-        super(capacityMultiplier, DyeBackedColor.fromDyeColor(DyeColor.BROWN));
-    }
-
-    @Override
-    public boolean isItemAllowedInContainer(ItemStack stackToAdd) {
-        return stackToAdd.is(BiggerFishTags.ALLOWED_IN_FISH_BARREL);
+    public FishBarrelProvider(int capacityMultiplier, StorageOptions storageOptions) {
+        super(capacityMultiplier, storageOptions);
     }
 
     @Override
     public boolean canAddItem(ItemStack containerStack, ItemStack stackToAdd, Player player) {
-        return super.canAddItem(containerStack, stackToAdd, player);
+        return stackToAdd.is(BiggerFishTags.ALLOWED_IN_FISH_BARREL) && super.canAddItem(containerStack, stackToAdd, player);
     }
 
     @Override
-    public Type getType() {
+    public ItemStorageType<?> getType() {
         return ItemInteractionsCompat.FISH_BARREL_ITEM_CONTENTS_PROVIDER_TYPE.value();
     }
 }
